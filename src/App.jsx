a150c9bff98880e4e32f662cb55d9d29b778e639
Nicolas Nuñez <nanvii213@gmail.com>
2024-05-01 14:22:11 -0300

    websocket

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,7 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const newSocket = io("https://dotsandboxeswsserver.glitch.me/", {
+    const newSocket = io("wss://dotsandboxeswsserver.glitch.me/", {
       autoConnect: true,
     });
     newSocket?.emit("req-to-play", playerName);
